1f729427b937de2ef4f53abc6c0d4d5fac7bd161
Jiwon Jung <jw.jeoung@albamapp.com>
2018-04-02 18:55:57 +0900

    Jiwon:: namespace $ --outFile app.js

---
+++ rectangleMath.js
@@ -0,0 +1,7 @@
+var MyMath;
+(function (MyMath) {
+    function calculateCircumference(diameter) {
+        return diameter * PI;
+    }
+    MyMath.calculateCircumference = calculateCircumference;
+})(MyMath || (MyMath = {}));
---
+++ rectangleMath.ts
@@ -0,0 +1,6 @@
+namespace MyMath {
+	export function calculateCircumference(diameter: number) {
+		return diameter * PI;
+	}
+}
+
