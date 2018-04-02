4c6468b1b0db09a66041cebbe849cf3164b73895
pi3rrot <4dakto@gmail.com>
2018-04-02 23:14:47 +0900

    Interaces and Properties

--- math/circle.ts
+++
@@ -1,5 +0,0 @@
-export const PI = 3.14;
-
-export function calcualteCirumcumference(diameter: number) {
-    return diameter * PI;
-}
--- math/rectangle.ts
+++
@@ -1,3 +0,0 @@
-export function calclulateRectangle(width: number, length: number) {
-    return width * length;
-}
