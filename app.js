15fcfad21d1d0e443ebedeb46a05506c82470536
pi3rrot <4dakto@gmail.com>
2018-04-08 21:42:35 +0900

    Using Interfaces to Describe Types in TypeScript

--- app.js
+++ app.js
@@ -1,38 +1,114 @@
-// Simple Gerneric
-function echo(data) {
-    return data;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+function logged(constructorFn) {
+    console.log(constructorFn);
 }
-console.log(echo("max").length);
-console.log(echo(27));
-console.log(echo({ name: "max", age: 27 }));
-// Better Generic
-function betterEcho(data) {
-    return data;
-}
-console.log(betterEcho("max").length);
-console.log(betterEcho(27));
-console.log(betterEcho({ name: "max", age: 27 }));
-// Built-in Gererics
-var testResults = [1.94, 2.33];
-testResults.push(-2.99);
-// Arrarys
-function pritAll(args) {
-    args.forEach((function (element) { return console.log(element); }));
-}
-pritAll(['Apple', 'Banana']);
-// Gerneric Types
-var echo2 = betterEcho;
-console.log(echo2('Somthing'));
-// Generic Class
-var SimpleMath = /** @class */ (function () {
-    function SimpleMath() {
+var Person = /** @class */ (function () {
+    function Person() {
+        console.log('Hi');
     }
-    SimpleMath.prototype.calculate = function () {
-        return +this.baseValue * +this.multiplyValue;
-    };
-    return SimpleMath;
+    Person = __decorate([
+        logged
+    ], Person);
+    return Person;
 }());
-var simpleMath = new SimpleMath();
-simpleMath.baseValue = '10';
-simpleMath.multiplyValue = 20;
-console.log(simpleMath.calculate());
+// Factory
+function logging(value) {
+    return value ? logged : null;
+}
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car = __decorate([
+        logging(true)
+    ], Car);
+    return Car;
+}());
+// Advanced
+function printable(constructorFn) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    };
+}
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this.name = 'Green Plant';
+    }
+    Plant = __decorate([
+        logging(true),
+        printable
+    ], Plant);
+    return Plant;
+}());
+var plant = new Plant();
+plant.print();
+// Method Decorator
+// Property Decorator
+function editable(value) {
+    return function (target, propName, descriptor) {
+        descriptor.writable = value;
+    };
+}
+function overwritable(value) {
+    return function (target) {
+        var newDescriptor = {
+            writable: value
+        };
+        return newDescriptor;
+    };
+}
+var Project = /** @class */ (function () {
+    function Project(name) {
+        this.projectName = name;
+    }
+    Project.prototype.calBuget = function () {
+        console.log(1000);
+    };
+    __decorate([
+        overwritable(false)
+    ], Project.prototype, "projectName", void 0);
+    __decorate([
+        editable(false)
+    ], Project.prototype, "calBuget", null);
+    return Project;
+}());
+var project = new Project("Super Project");
+project.calBuget();
+project.calBuget = function () {
+    console.log(2000);
+};
+project.calBuget();
+console.log(project);
+// Parameter Decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log('Target: ', target);
+    console.log('methodName: ', methodName);
+    console.log('paramIndex: ', paramIndex);
+}
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = name;
+    }
+    Course.prototype.printStudentNumbers = function (mode, printAll) {
+        if (printAll) {
+            console.log(10000);
+        }
+        else {
+            console.log(2000);
+        }
+    };
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumbers", null);
+    return Course;
+}());
+var course = new Course('Super Course');
+course.printStudentNumbers('anything', true);
+course.printStudentNumbers('anything', false);
